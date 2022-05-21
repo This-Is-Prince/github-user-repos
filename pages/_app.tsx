7dad8b9d828580da9fcfb304695a28734726ec8c
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-21 19:24:05 +0530

    context removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import AppWrapper from "../context/AppContext";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppWrapper>
+    <>
       <Header />
       <Component {...pageProps} />
-    </AppWrapper>
+    </>
   );
 }
 
