bffebaa8d57685ea508bd0df1cb520382ed5397b
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-21 22:23:45 +0530

    console log removed from normal

--- components/pagination/page/Normal.tsx
+++ components/pagination/page/Normal.tsx
@@ -9,7 +9,6 @@ const Normal: FC<{ value: number }> = ({ value }) => {
       className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
       onClick={(e) => {
         e.preventDefault();
-        console.log(router.query);
         const { name } = router.query;
         router.push(`/users/${name}/repos?page=${value}&per_page=10`);
       }}
